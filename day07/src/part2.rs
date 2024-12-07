c2c0f150793ca1604b4e1b9ea78e72a3c5a31d04
valentinefleith <valentine.fleith@gmail.com>
2024-12-07 19:34:27 +0100

    ugly but optimized

--- day07/src/part2.rs
+++ day07/src/part2.rs
@@ -5,31 +5,32 @@
 //    concatenated_str.parse::<u64>().unwrap()
 //}
 
-fn concatenate(uint1: u64, uint2: u64) -> u64 {
-    let digits = (uint2 as f64).log10().floor() as u32 + 1;
+fn concatenate(uint1: &u64, uint2: &u64) -> u64 {
+    let digits = (*uint2 as f64).log10().floor() as u32 + 1;
     uint1 * 10u64.pow(digits) + uint2
 }
 
-fn is_combination(test: &u64, remaining: Vec<u64>, current_result: u64) -> bool {
-    if remaining.is_empty() {
-        return current_result == *test;
+fn is_combination(test: &u64, remaining: &Vec<u64>, current_result: &u64, n: usize) -> bool {
+    if n == remaining.len() {
+        return current_result == test;
     }
-    if current_result > *test {
+    if current_result > test {
         return false;
     }
-    is_combination(test, remaining[1..].to_vec(), current_result + remaining[0])
-        || is_combination(test, remaining[1..].to_vec(), current_result * remaining[0])
+    is_combination(test, &remaining, &(current_result + remaining[n]), n + 1)
+        || is_combination(test, &remaining, &(current_result * remaining[n]), n + 1)
         || is_combination(
             test,
-            remaining[1..].to_vec(),
-            concatenate(current_result, remaining[0]),
+            &remaining,
+            &concatenate(&current_result, &remaining[n]),
+            n + 1,
         )
 }
 
 pub fn get_right_combinations2(tested: Vec<u64>, remaining: Vec<Vec<u64>>) -> u64 {
     let mut result: u64 = 0;
     for (i, test) in tested.iter().enumerate() {
-        if is_combination(test, remaining[i][1..].to_vec(), remaining[i][0]) {
+        if is_combination(test, &remaining[i], &remaining[i][0], 1) {
             result += test;
         }
     }

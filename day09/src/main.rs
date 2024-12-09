c2a8667089e37b39cd9fc8ae0e0038f8d76ab36a
valentinefleith <valentine.fleith@gmail.com>
2024-12-09 20:38:11 +0100

    add day09  part2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,11 +1,13 @@
+//use std::env;
 use std::fs;
 
 use day09::part1::get_compact_files;
 use day09::part1::get_final_result;
 use day09::part1::line_to_blocks;
+use day09::part2::get_compact_p2;
 
 const INPUT_PATH: &str = "input.txt";
-const PART: u32 = 1;
+const PART: u32 = 2;
 
 fn read_input(filename: &str) -> String {
     fs::read_to_string(filename)
@@ -15,9 +17,16 @@ fn read_input(filename: &str) -> String {
 }
 
 fn main() {
+    //env::set_var("RUST_BACKTRACE", "1");
     let line = read_input(INPUT_PATH);
     let block_representation = line_to_blocks(line);
-    let compact = get_compact_files(block_representation);
+    //println!("{:?}", block_representation);
+    let compact = if PART == 1 {
+        get_compact_files(block_representation)
+    } else {
+        get_compact_p2(block_representation)
+    };
+    println!("COMPACT = {:?}", compact);
     let result = get_final_result(compact);
     println!("{}", result);
 }
---
+++ day09/src/part2.rs
@@ -0,0 +1,95 @@
+const EMPTY: i32 = -1;
+
+fn compute_empty_len(blocks: &Vec<i32>, index: usize) -> usize {
+    let mut count = 0;
+    while index + count < blocks.len() && blocks[index + count] == EMPTY {
+        count += 1;
+    }
+    count
+}
+
+fn compute_nb_len(blocks: &Vec<i32>, index: usize) -> usize {
+    let mut count = 0;
+    while count <= index && blocks[index - count] == blocks[index] {
+        count += 1;
+    }
+    count
+}
+
+fn get_next_nb_index(blocks: &Vec<i32>, mut index: usize) -> usize {
+    while index >= 0 && blocks[index] == EMPTY {
+        index -= 1;
+    }
+    index
+}
+
+pub fn get_compact_p2(mut blocks: Vec<i32>) -> Vec<i32> {
+    let mut j: usize = blocks.len() - 1;
+    while j >= 0 {
+        let current_nb_len = compute_nb_len(&blocks, j);
+        let mut i: usize = 0;
+        while i < j {
+            if blocks[i] == EMPTY {
+                let empty_len = compute_empty_len(&blocks, i);
+                if empty_len < current_nb_len {
+                    i += empty_len;
+                    continue;
+                }
+                let mut count = 0;
+                while count < current_nb_len {
+                    if blocks[j] == 9999 {}
+
+                    blocks[i + count] = blocks[j - count];
+                    blocks[j - count] = -1;
+                    count += 1;
+                }
+                break;
+            }
+            i += 1;
+        }
+        if j > current_nb_len {
+            j = get_next_nb_index(&blocks, j - current_nb_len);
+        } else {
+            break;
+        }
+    }
+    blocks
+}
+
+//pub fn get_compact_p2(mut blocks: Vec<i32>) -> Vec<i32> {
+//let mut i: usize = 0;
+//let mut j: usize = blocks.len() - 1;
+
+//while i < blocks.len() {
+//println!("{:?}", blocks);
+//if blocks[i] == EMPTY {
+//let empty_len = compute_empty_len(&blocks, i);
+//let mut current_nb_len = compute_nb_len(&blocks, j);
+//println!("current nb_len = {}", current_nb_len);
+//println!("empy _len = {}", empty_len);
+//println!(".. j corresponds to {}", j);
+//while j > 0 && compute_nb_len(&blocks, j) > empty_len {
+//j = get_next_nb_index(&blocks, j - current_nb_len);
+//println!("searching for next nb.. j corresponds to {}", j);
+//current_nb_len = compute_nb_len(&blocks, j);
+//println!("current nb_len = {}", current_nb_len);
+//}
+//if j == 0 {
+//return blocks;
+//}
+//let mut count = 0;
+//while count < current_nb_len {
+//blocks[i + count] = blocks[j - count];
+//blocks[j - count] = -1;
+//count += 1;
+//println!("just been swapped");
+//}
+//i += empty_len;
+//println!("i corresponds to {}", blocks[i]);
+//j = get_next_nb_index(&blocks, j - current_nb_len);
+//println!("j corresponds to {}", blocks[j]);
+//}
+//i += 1;
+//}
+//blocks
+//}
